6c44fda26dedcfe7aacb5e2670f0ca4aaab5ad70
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-07 12:39:58 +0300

    Add files via upload

---
+++ Services/Concrete/ArticleService.cs
@@ -0,0 +1,24 @@
+﻿using BlogProject.Data.UnitOfWorks;
+using BlogProject.Entity.Entities;
+using BlogProject.Service.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Service.Services.Concrete
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public ArticleService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public async Task<List<Article>> GetAllArticlesAsync()
+        {
+            return await unitOfWork.GetRepository<Article>().GetAllAsync();
+        }
+    }
+}
