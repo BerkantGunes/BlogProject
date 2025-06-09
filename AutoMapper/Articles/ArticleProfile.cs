d40b0b04b69d88e8b85535c723e7337edc455f48
Berkant Gunes <gunesberkant96@gmail.com>
2025-06-09 19:16:32 +0300

    Add files via upload

---
+++ AutoMapper/Articles/ArticleProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BlogProject.Entity.Entities;
+using BlogProject.Entity.ViewModels.Articles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Service.AutoMapper.Articles
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            CreateMap<ArticleDto, Article>().ReverseMap();
+        }
+    }
+}
